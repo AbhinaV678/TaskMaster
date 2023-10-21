302a67eb996a0c05b5734c2280ecd1624acbf69a
ABHINAV <debmickey4@gmail.com>
2023-10-21 21:50:33 +0530

    addition of update functionality to all lists.

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const item2 = new Item({
 });
 
 const item3 = new Item({
-  itemName : "Use the checkbox beside each task by ticking it to delete the particular task.",
+  itemName : "Use the checkbox beside each task by checking it to delete the particular task.",
 });
 
 const item4 = new Item({
@@ -44,11 +44,11 @@ const item5 = new Item({
 });
 
 const item6 = new Item({
-  itemName : "If you want to see the names of lists you have created visit 'websiteUrl/data' , you can delete the lists there by checking the checkboxes.",
+  itemName : "If you want to see the names of lists you have created visit 'websiteUrl/data' , you can delete the lists by checking the checkboxes there.",
 });
 
 const item7 = new Item({
-  itemName : "Don't submit blank entries!",
+  itemName : "if you don't see your entry updated after a particular operation , don't worry - just refresh the page.",
 });
 
 const defaultItems = [item1,item2,item3,item4,item5,item6,item7];
@@ -98,10 +98,22 @@ app.get("/:customListName",async function(req,res){
       }
 });
 
+//Create Task
 app.post("/", async function(req, res){
 
   const name = req.body.newItem;
   const listName = req.body.list;
+
+  if(name.trim()===""){
+    console.log("Blank Entry!");
+    if(listName==="Today"){
+      return res.redirect("/");
+    }
+    else{
+      return  res.redirect("/" + listName);
+    }
+  }
+
   const item = new Item({
     itemName : name,
   });
@@ -118,28 +130,74 @@ app.post("/", async function(req, res){
   }
 });
 
+//Update Task
+app.post("/update", async function(req, res){
+
+  const updatedItem = req.body.editedItem;
+  const updateItemId = req.body.edit;
+  const listName = req.body.listName;
+
+  if(updatedItem.trim()===""){
+    console.log("Blank Update Entry!");
+    if(listName==="Today"){
+      return res.redirect("/");
+    }
+    else{
+      return  res.redirect("/" + listName);
+    }
+  }
+
+  if(listName==="Today"){
+    try {
+      await Item.findByIdAndUpdate(updateItemId, { $set: { itemName: updatedItem }});
+    } catch (error) {
+      console.log(error);
+    }
+    return res.redirect("/");
+  }else{
+    try {
+      await List.findOneAndUpdate({name: listName, "items._id" : updateItemId}, {$set: {"items.$.itemName": updatedItem}});
+    } catch (error) {
+      console.log(error);
+    }
+    return res.redirect("/" + listName);
+  }
+});
+
+//delete Task
 app.post("/delete", async function(req, res){
  const deleteItemId = req.body.deletedItem;
  const listName = req.body.listName;
   if(listName==="Today"){
-    await Item.findByIdAndDelete(deleteItemId);
+    try {
+      await Item.findByIdAndDelete(deleteItemId);
+    } catch (error) {
+      console.log(error);
+    }
     res.redirect("/");
   }else{
-    var foundList = await List.findOneAndUpdate({name: listName},{$pull:{items:{_id: deleteItemId}}});
-    if(foundList){
-      foundList.save();
-      res.redirect("/" + listName);
-    }else{
-      res.send({message: "Error!"});
+    try {
+      var foundList = await List.findOneAndUpdate({name: listName},{$pull:{items:{_id: deleteItemId}}});
+      if(foundList){
+        foundList.save();
+      }
+    } catch (error) {
+      console.log(error);
     }
+    res.redirect("/" + listName);
   }
 });
 
-
+//delete List
 app.post("/deleteList", async function(req, res){
   const deleteItemId = req.body.deletedList;
-  var listName = "Data";
-  await List.findByIdAndDelete(deleteItemId);
+  const listName = "Data";
+
+  try {
+    await List.findByIdAndDelete(deleteItemId);
+  } catch (error) {
+    console.log(error);
+  }
   res.redirect("/" + listName);
  });
 
@@ -151,3 +209,21 @@ if(port==null || port == ""){
 app.listen(port, function() {
   console.log("Server started successfully.");
 });
+
+
+ 
+
+/*
+if this type of error  hits :
+
+Error: querySrv ETIMEOUT _mongodb._tcp.cluster0.yacgh7z.mongodb.net
+    at QueryReqWrap.onresolve [as oncomplete] (node:internal/dns/promises:252:17) {
+  errno: undefined,
+  code: 'ETIMEOUT',
+  syscall: 'querySrv',
+  hostname: '_mongodb._tcp.cluster0.yacgh7z.mongodb.net'
+}
+
+Then :-  connect your system to your mobile hotspot instead of using wifi, sometimes wifi connections are configured to block these connection requests from Atlas.
+
+*/
